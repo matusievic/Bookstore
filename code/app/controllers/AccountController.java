4e965ac47c02b31349fcf978eba44978cd8d9d71
Andrei Matusevich <matusievic@gmail.com>
2017-11-15 21:26:39 +0300

    Initial code commit

---
+++ code/app/controllers/AccountController.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.*;
+import play.mvc.*;
+
+import views.html.*;
+
+public class AccountController extends Controller {
+    public Result login() {
+        
+    }
+}
