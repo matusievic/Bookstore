53594acc54ca49f19fbaafb3e97325adbefe7fa9
Andrei Matusevich <matusievic@gmail.com>
2017-11-17 22:16:59 +0300

    Database plugins were configured

--- code/build.sbt
+++ code/build.sbt
@@ -8,6 +8,11 @@ resolvers += "scalaz-bintray" at "https://dl.bintray.com/scalaz/releases"
       
 scalaVersion := "2.11.11"
 
-libraryDependencies ++= Seq( javaJdbc , cache , javaWs )
+libraryDependencies ++= Seq( javaJdbc , cache , javaWs)
 
 unmanagedResourceDirectories in Test <+=  baseDirectory ( _ /"target/web/public/test" )
+
+lazy val myProject = (project in file(".")).enablePlugins(PlayJava, PlayEbean)
+
+libraryDependencies += "mysql" % "mysql-connector-java" % "5.1.24"
+libraryDependencies += guice
---
+++ code/conf/evolutions/default/1.sql
@@ -0,0 +1,20 @@
+# --- Created by Ebean DDL
+# To stop Ebean DDL generation, remove this comment and start using Evolutions
+
+# --- !Ups
+
+create table user (
+  email                         varchar(255) not null,
+  password                      varchar(255),
+  name                          varchar(255),
+  surname                       varchar(255),
+  type                          integer,
+  constraint ck_user_type check ( type in (0,1)),
+  constraint pk_user primary key (email)
+);
+
+
+# --- !Downs
+
+drop table if exists user;
+
